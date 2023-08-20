2fd66e7cc0920b0eb5d574d4ecd88d99c8050e1a
Vladimir Gergert <pigunderwater@gmail.com>
2023-08-20 21:01:59 +0600

    chore: move IssueItem to components/

---
+++ src/components/IssueItem/IssueItem.tsx
@@ -0,0 +1,45 @@
+import {
+  Avatar,
+  List,
+  Skeleton,
+  Space,
+  Tag,
+} from 'antd';
+import { IIssue } from '../../api/issues.api';
+import calculateColor from '../../utils/calculateColor';
+import './IssueItem.scss';
+
+export default function IssueItem(item: IIssue, loading: boolean) {
+  const {
+    user, title, labels, number,
+  } = item;
+
+  return (
+    <List.Item>
+      <Skeleton title={false} loading={loading} active>
+        <List.Item.Meta
+          className="issue__list_item"
+          avatar={<Avatar src={user.avatar_url} />}
+          title={title}
+          description={(
+            labels.map(
+              (label) => (
+                <Tag
+                  key={label.id}
+                  color={`#${label.color}`}
+                  style={{ color: calculateColor(label.color) }}
+                >
+                  { label.name }
+                </Tag>
+              ),
+            )
+          )}
+        />
+
+        <Space>
+          { `#${number}` }
+        </Space>
+      </Skeleton>
+    </List.Item>
+  );
+}
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,12 +1,7 @@
-import {
-  Avatar,
-  Space,
-  List, Skeleton,
-  Tag,
-} from 'antd';
-import IssuesApiProvider, { IIssue } from '../../api/issues.api';
+import { List } from 'antd';
+import IssuesApiProvider from '../../api/issues.api';
+import IssueItem from '../../components/IssueItem/IssueItem';
 import './HomePage.scss';
-import calculateColor from '../../utils/calculateColor';
 
 function HomePage() {
   const issuesApiProvider = new IssuesApiProvider();
@@ -14,41 +9,12 @@ function HomePage() {
 
   const [data, loading] = issuesApiProvider.list(repository);
 
-  const itemListRenderer = (item: IIssue) => (
-    <List.Item>
-      <Skeleton title={false} loading={loading} active>
-        <List.Item.Meta
-          className="home_page__list_item"
-          avatar={<Avatar src={item.user.avatar_url} />}
-          title={item.title}
-          description={(
-            item.labels.map(
-              (label) => (
-                <Tag
-                  key={label.id}
-                  color={`#${label.color}`}
-                  style={{ color: calculateColor(label.color) }}
-                >
-                  { label.name }
-                </Tag>
-              ),
-            )
-          )}
-        />
-
-        <Space>
-          { `#${item.number}` }
-        </Space>
-      </Skeleton>
-    </List.Item>
-  );
-
   return (
     <List
       className="home_page__list"
       loading={loading}
       dataSource={data}
-      renderItem={itemListRenderer}
+      renderItem={(item) => IssueItem(item, loading)}
     />
   );
 }
