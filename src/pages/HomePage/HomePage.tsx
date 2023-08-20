348cb4f5c98f7af12d126ad54287284d9adc955d
Vladimir Gergert <pigunderwater@gmail.com>
2023-08-21 01:09:16 +0600

    feat: improve pagination

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,34 +1,65 @@
 import { List } from 'antd';
 import { PaginationConfig } from 'antd/es/pagination';
 import useAxios from 'axios-hooks';
+import { useState } from 'react';
 import IssueItem from '../../components/IssueItem/IssueItem';
-import './HomePage.scss';
 import { IIssue } from '../../types/issues';
+import IRepository from '../../types/repositories';
+import './HomePage.scss';
+
+const DEFAULT_PAGE_SIZE = 15;
 
 function HomePage() {
-  const repository = { repositoryName: 'facebook/react' };
+  const [page, setPage] = useState<number>(1);
 
-  const [{ data: issues, loading }] = useAxios<IIssue[]>(
-    `/repos/${repository.repositoryName}/issues`,
+  const searchOptions = { repositoryName: 'facebook/react' };
+
+  // Making request to obtain issues amount for pagination
+  const [{
+    data: issues,
+    loading: issuesLoading,
+  }] = useAxios<IIssue[]>({
+    url: `/repos/${searchOptions.repositoryName}/issues`,
+    params: {
+      page,
+      per_page: DEFAULT_PAGE_SIZE,
+      opened: true,
+    },
+  });
+
+  // Making request to obtain all issues of the repository
+  const [{
+    data: repository,
+    loading: repositoryLoading,
+  }] = useAxios<IRepository>(
+    `/repos/${searchOptions.repositoryName}`,
   );
 
   const paginationSettings: PaginationConfig = {
+    className: 'home_page__pagination',
     position: 'top',
-    align: 'center',
-    pageSize: 15,
-    // total: 300,
+    align: 'start',
+
+    pageSize: DEFAULT_PAGE_SIZE,
+    total: repositoryLoading
+      ? DEFAULT_PAGE_SIZE
+      : repository?.open_issues_count,
+
     showSizeChanger: false,
-    // onChange(page, pageSize) {
-    // },
+
+    onChange(pageNumber: number) {
+      setPage(pageNumber);
+    },
   };
 
   return (
     <List
+      bordered
       pagination={paginationSettings}
       className="home_page__list"
-      loading={loading}
+      loading={issuesLoading}
       dataSource={issues}
-      renderItem={(item) => IssueItem(item, loading)}
+      renderItem={IssueItem}
     />
   );
 }
